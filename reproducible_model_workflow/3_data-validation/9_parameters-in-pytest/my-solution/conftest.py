c1bac86bcf68506ceba4c07c99be961d177507b4
yurimarca <yurimarca@gmail.com>
2024-11-26 19:28:18 -0300

    Exercise 9 done

--- reproducible_model_workflow/3_data-validation/9_parameters-in-pytest/my-solution/conftest.py
+++ reproducible_model_workflow/3_data-validation/9_parameters-in-pytest/my-solution/conftest.py
@@ -11,7 +11,7 @@ def pytest_addoption(parser):
     parser.addoption("--sample_artifact", action="store")
 
     # COMPLETE HERE: add the option for ks_alpha
-
+    parser.addoption("--ks_alpha", action="store")
 
 @pytest.fixture(scope="session")
 def data(request):
@@ -40,3 +40,10 @@ def ks_alpha(request):
 
     # COMPLETE HERE: read the option ks_alpha from the command line,
     # and return it as a float
+
+    ks_alpha = request.config.option.ks_alpha
+    
+    if ks_alpha is None:
+        pytest.fail("--ks_alpha missing on command line.")
+
+    return float(ks_alpha)
---
+++ reproducible_model_workflow/3_data-validation/9_parameters-in-pytest/my-solution/mlflow_run.sh
@@ -0,0 +1,1 @@
+mlflow run . -P reference_artifact="exercise6/data_train.csv:latest" -P sample_artifact="exercise_6/data_test.csv:latest" -P ks_alpha=0.05
